57535fff9201b5b4fedace04d72f8cff2c3d8394
BrainOfTech <ssaarinen@live.com>
2016-04-06 12:35:43 -0400

    Added code folder to hold analysis script. Tweaked for correctness.

---
+++ Code/AnalyzeRoute.py
@@ -0,0 +1,6 @@
+#This file last edited by (S)am on 4-6-16
+#This file contains utilities for parsing input from the encrypted email,
+#analyzing coordinate streams,
+#maintaining a list of known locations and paths,
+#generating encrypted receipt pages for use by the mobile application,
+#and entering data into the relevant databases.
